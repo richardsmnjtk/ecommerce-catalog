41df08eca3957489993a9b4a2b2da3ad8d1669d4
Richard Simanjuntak <72237847+richardsmnjtk@users.noreply.github.com>
2023-10-25 10:07:26 +0700

    init: FE Project Core Initiative

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ProductDisplay/>
+</template>
+
+<script>
+import ProductDisplay from './components/ProductDisplay.vue'
+
+export default {
+  components: {
+    ProductDisplay
+  }
+}
+</script>
